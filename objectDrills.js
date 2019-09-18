4096cf90807cbaec73143460c04f5b6092eea9da
danWagar <dandwagar@gmail.com>
2019-09-18 11:19:32 -0700

    employee and boss

--- objectDrills.js
+++ objectDrills.js
@@ -27,8 +27,24 @@ let obj = {
 
 console.log(obj.meals[3]);
 
-let employee = [{name: 'heesu', jobTitle: 'student'}, {name: 'Dan', jobTitle: 'student'},
-{name: 'amy', jobTitle: 'student'}];
-for(let i=0; i<employee.length; i++) {
+let employee = [
+  { name: 'heesu', jobTitle: 'student' },
+  { name: 'Dan', jobTitle: 'student' },
+  { name: 'amy', jobTitle: 'student' }
+];
+for (let i = 0; i < employee.length; i++) {
   console.log(employee[i].name + ' ' + employee[i].jobTitle);
-};
+}
+
+//add Boss key
+for (let i = 0; i < employee.length; i++) {
+  employee[i].boss = 'andrea';
+}
+
+employee.push({ name: 'andrea', jobTitle: 'Teacher' });
+
+for (let i = 0; i < employee.length; i++) {
+  if (employee[i].hasOwnProperty('boss'))
+    console.log(employee[i].jobTitle + ' ' + employee[i].name + ' reports to ' + employee[i].boss + '.');
+  else console.log(`${employee[i].jobTitle} ${employee[i].name} doesn't report to anybody.`);
+}
