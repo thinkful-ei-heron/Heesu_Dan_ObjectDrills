329bd4293a1f93fd71fee59d64e7669e6457cc58
danWagar <dandwagar@gmail.com>
2019-09-18 12:33:31 -0700

    lotr

--- objectDrills.js
+++ objectDrills.js
@@ -50,25 +50,69 @@ for (let i = 0; i < employee.length; i++) {
 }
 
 let cipher = {
-    a:2,
-    b:3,
-    c:4,
-    d:5
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5
 };
 let test = 'craft block argon meter bells brown croon droop'.split(' ');
 
 function decode(encode) {
   let word = '';
-  for(let i=0; i<encode.length; i++) {
+  for (let i = 0; i < encode.length; i++) {
     let first = encode[i].charAt(0);
-    if(cipher.hasOwnProperty(first)) {
-      word += encode[i].charAt(cipher[first]-1);
-    }
-    else {
+    if (cipher.hasOwnProperty(first)) {
+      word += encode[i].charAt(cipher[first] - 1);
+    } else {
       word += ' ';
     }
   }
   return word;
 }
 
-console.log (decode(test));
+console.log(decode(test));
+
+function lotr(name, nickname, race, origin, attack, defense, weapon) {
+  return {
+    name: name,
+    nickname: nickname,
+    race: race,
+    origin: origin,
+    attack: attack,
+    defense: defense,
+    weapon: weapon,
+    describe: function() {
+      return `${this.name} is a ${this.race} from ${this.origin} who uses a ${this.weapon}`;
+    },
+    evaluateFight: function(char) {
+      let x = 0;
+      let y = 0;
+      if (char.defense > this.attack) x = 0;
+      else x = this.attack - char.defense;
+      if (this.defense > char.attack) y = 0;
+      else y = char.attack - this.defense;
+      return `Your opponent takes ${x} damage and you receive ${y} damage`;
+    }
+  };
+}
+
+let characters = [
+  lotr('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'staff'),
+  lotr('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'ring'),
+  lotr('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2, 'Barrow Blade'),
+  lotr('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8, 'Anduril'),
+  lotr('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5, 'Bow and Arrow'),
+  lotr('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 1, 1, 'Hadhafang')
+];
+
+//console.log(characters[3]);
+
+let aragorn = characters.find(el => el.nickname === 'aragorn');
+
+console.log(aragorn.describe());
+
+let hobbits = characters.filter(el => el.race === 'Hobbit');
+
+let attackGT5 = characters.filter(el => el.attack > 5);
+
+console.log(attackGT5);
