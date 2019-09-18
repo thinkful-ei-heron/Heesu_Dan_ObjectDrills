35d0b234e62fe303896bc4dafd3ff369fcea3ee1
Heesu Student <hskang135@gmail.com>
2019-09-18 12:13:22 -0400

    delete obj

--- drill1.js
+++ drill1.js
@@ -30,6 +30,46 @@ function personMaker() {
   return person;
 }
 
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+(function testKeyDeleter() {
+  var obj = keyDeleter({
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  });
+
+  if (typeof obj !== 'object') {
+    console.error('ERROR: `keyDeleter` must be return an object');
+    return false;
+  }
+  ['foo', 'bar'].forEach(function(key) {
+    if (key in obj) {
+      console.error('`keyDeleter` did not delete the key for ' + key);
+      return false;
+    }
+  });
+  ['bizz', 'bang'].forEach(function(key) {
+    if (!(key in obj && obj[key] === key)) {
+      console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
+      return false;
+    }
+  });
+  console.log('SUCCESS: `keyDeleter` works correctly!');
+})();
+
 (function testPersonMaker() {
   var person = personMaker();
   if (typeof person !== 'object') {
