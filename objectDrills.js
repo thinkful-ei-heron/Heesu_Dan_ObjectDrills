9d4aaf81c7cd1bc1e4d444665d499bb642f68198
danWagar <dandwagar@gmail.com>
2019-09-18 10:59:11 -0700

    hobbit meal

--- objectDrills.js
+++ objectDrills.js
@@ -10,14 +10,19 @@ loaf.hydration = function() {
 console.log(loaf.hydration());
 
 let object = {
-    foo: 1,
-    bar: 2,
-    fum: 3,
-    quux: 4,
-    spam: 5
+  foo: 1,
+  bar: 2,
+  fum: 3,
+  quux: 4,
+  spam: 5
 };
 
-for(let key in object) {
-  console.log(object[key])
+for (let key in object) {
+  console.log(object[key]);
+}
+
+let obj = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 };
 
+console.log(obj.meals[3]);
